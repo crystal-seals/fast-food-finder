e1061898fe8fe78e09ef254222491bf9b0f0b4ac
TroyMaeder <troy.maeder@gmail.com>
2016-06-22 16:18:26 +0100

    Add functionality for 20 restaurants

--- restaturants.js
+++ restaturants.js
@@ -4,22 +4,40 @@ var newObj = {};
 
 var xhr = new XMLHttpRequest();
 
+function updateObject() {
+  var result = JSON.parse(xhr.response)
+  var input = result.restaurants[count].restaurant;
+  newObj.name = input.name
+  newObj.id = input.id;
+  newObj.thumb = input.thumb
+  newObj.cuisines = input.cuisines
+  newObj.rating = input.user_rating.aggregate_rating;
+  newObj.address = input.location.address;
+  update(0);
+  count++;
+}
 
+  var count = 0;
   xhr.onreadystatechange = function() {
     if (xhr.readyState === 4 && xhr.status === 200) {
-      var result = JSON.parse(xhr.response)
-      var input = result.restaurants[0].restaurant;
-      newObj.name = input.name
-      newObj.id = input.id;
-      newObj.thumb = input.thumb
-      newObj.cuisines = input.cuisines
-      newObj.rating = input.user_rating.aggregate_rating;
-      newObj.address = input.location.address;
-      update(0);
+      updateObject();
     }
   }
-xhr.open('GET', 'https://developers.zomato.com/api/v2.1/search?count=1&lat=51.5290734&lon=-0.0442646&radius=1000&cuisines=40&sort=real_distance&apikey=3c2968b8e9cb6e81212628d734ccb726', true)
+xhr.open('GET', 'https://developers.zomato.com/api/v2.1/search?count=20&lat=51.5290734&lon=-0.0442646&radius=1000&cuisines=40&sort=real_distance&apikey=3c2968b8e9cb6e81212628d734ccb726', true)
 xhr.send();
+
+document.getElementById("button").addEventListener("click", function() {
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      updateObject();
+      if (count === 19) {
+        count = 0;
+      }
+    }
+  }
+  xhr.open('GET', 'https://developers.zomato.com/api/v2.1/search?count=20&lat=51.5290734&lon=-0.0442646&radius=1000&cuisines=40&sort=real_distance&apikey=3c2968b8e9cb6e81212628d734ccb726', true)
+  xhr.send();
+});
 
 function update(num) {
   document.getElementById("image" + num).style.backgroundImage = "url(" + newObj.thumb + ")"
