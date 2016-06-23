be8b4bebf208a7c92a51e3259d00427d7958c7f1
Richard Warren <rwarren92@hotmail.com>
2016-06-23 14:28:20 +0100

    lkajdsfa

--- restaturants.js
+++ restaturants.js
@@ -4,6 +4,7 @@ var currentRestaurant = {};
 document.getElementById('button').addEventListener("click", function() {
   document.getElementById('homepage').style.display = "none";
   document.getElementById('results').style.display = "block";
+  getRestaurants();
 })
 
 var xhr = new XMLHttpRequest();
@@ -24,13 +25,15 @@ function updateRestaurant() {
 }
 
   var count = 0;
-  xhr.onreadystatechange = function() {
+  function getRestaurants() {
+    xhr.onreadystatechange = function() {
     if (xhr.readyState === 4 && xhr.status === 200) {
       updateRestaurant();
     }
   }
-  xhr.open('GET', 'https://developers.zomato.com/api/v2.1/search?count=20&lat=51.5290734&lon=-0.0442646&radius=1000&cuisines=40&sort=real_distance&apikey=3c2968b8e9cb6e81212628d734ccb726', true)
+  xhr.open('GET', 'https://developers.zomato.com/api/v2.1/search?count=20&lat=' + latitude + "&lon=" + longitude + "&radius=1000&cuisines=40&sort=real_distance&apikey=3c2968b8e9cb6e81212628d734ccb726", true)
   xhr.send();
+}
 
 document.getElementById("button").addEventListener("click", function() {
   xhr.onreadystatechange = function() {
@@ -41,7 +44,7 @@ document.getElementById("button").addEventListener("click", function() {
       }
     }
   }
-  xhr.open('GET', 'https://developers.zomato.com/api/v2.1/search?count=20&lat=51.5290734&lon=-0.0442646&radius=1000&cuisines=40&sort=real_distance&apikey=3c2968b8e9cb6e81212628d734ccb726', true)
+  xhr.open('GET', 'https://developers.zomato.com/api/v2.1/search?count=20&lat=' + latitude + "&lon=" + longitude + "&radius=1000&cuisines=40&sort=real_distance&apikey=3c2968b8e9cb6e81212628d734ccb726", true)
   xhr.send();
 });
 
