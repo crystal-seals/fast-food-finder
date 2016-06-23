aaeae7eac581eb37289e6cca054f62f595364ae8
Heather <hdavidson94@gmail.com>
2016-06-23 16:23:02 +0100

    Cleans up file structure

--- new.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("image").style.backgroundImage = "url(" + currentRestaurant.thumb + ")"
-  document.getElementById("cuisine").innerHTML = currentRestaurant.cuisines;
-  document.getElementById("title").innerHTML = currentRestaurant.name;
-  document.getElementById("rating").innerHTML = currentRestaurant.rating + '/5';
