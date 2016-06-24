75547cf517da0ec8c5e029159a77761229e79522
Sofia <emmapohjalainen@gmail.com>
2016-06-24 10:40:51 +0100

    changed zomato api to load only one restaurant at a time

--- js/restaurants.js
+++ js/restaurants.js
@@ -63,7 +63,7 @@ function updateRestaurant() {
   }
 
   console.log('fetching restaurants from zomato', latitude, longitude);
-  xhr.open('GET', "https://developers.zomato.com/api/v2.1/search?count=20&lat=" + latitude + "&lon=" + longitude + "&radius=1000&cuisines=40&sort=real_distance&apikey=3c2968b8e9cb6e81212628d734ccb726", true)
+  xhr.open('GET', "https://developers.zomato.com/api/v2.1/search?count=1&start=1&lat=" + latitude + "&lon=" + longitude + "&radius=1000&cuisines=40&sort=real_distance&apikey=3c2968b8e9cb6e81212628d734ccb726", true)
   xhr.send();
 }
 document.getElementById('button').style.display = 'block';
